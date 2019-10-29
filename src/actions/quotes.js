6094a47e829cd3115f5a5d12986f60fa6491e7ba
dclements9 <dclements@protonmail.com>
2019-10-29 12:57:51 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,8 @@
 // TODO: Create action creators as defined in tests
+const addQuote = q => {
+    return {
+        type: 'ADD_QUOTE'
+        // quote: 
+    }
+}
+export {addQuote}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,25 @@
 export default (state = [], action) => {
-  return state;
+
+  let quote
+
+  switch (action.type){
+
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    
+    case 'UPVOTE_QUOTE':
+      return 'upvote'
+    
+    case 'DOWNVOTE QUOTE':
+      return 'downvote'  
+    
+    default: 
+        return state
+  }
+
+  // return state;
 }
+ 
